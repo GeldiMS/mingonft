3c5c0d4f3fa017e7421fe47285eec63f242dc008
GeldiMS <mansakugeldi@gmail.com>
2021-09-16 13:27:57 +0200

    removed useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Gallery from "./Gallery";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactGA from "react-ga";
-import { useEffect } from "react";
 
 function App() {
   const TRACKING_ID = "UA-207768070-1";
