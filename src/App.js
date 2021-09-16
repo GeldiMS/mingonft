22e944d4a65682914102090d6abbb3eafea5aae3
GeldiMS <mansakugeldi@gmail.com>
2021-09-16 12:44:39 +0200

    Added support for google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Gallery from "./Gallery";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ReactGA from "react-ga";
 
 function App() {
+  const TRACKING_ID = "286612884";
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <div>
       <style>{"body { background-color: #182029; }"}</style>
