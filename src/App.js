6e95725b31b6c906ac8e678e2a9c4acd68138671
GeldiMS <mansakugeldi@gmail.com>
2021-09-16 13:05:40 +0200

    ga added pageview

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import Gallery from "./Gallery";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 function App() {
   const TRACKING_ID = "UA-207750898";
-  ReactGA.initialize(TRACKING_ID);
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.pageview("/");
+  }, []);
 
   return (
     <div>
